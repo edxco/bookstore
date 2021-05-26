98181255a94f7afa9f77cfd257cf33cddde73a63
tavo <gasb_150@hotmail.com>
2021-05-26 17:54:52 -0500

    Fix linters

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,31 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const BooksList = ({books}) => {
-  console.log(books);
-  return (
+const BooksList = ({ books }) => (
 
-    <table>
-      <thead>
-        <tr>
-          <th>Book Id</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-        <tr>
-          {books.map((book) => (
-            <>
-              <td>{book.id}</td>
-              <td>{book.title}</td>
-              <td>{book.category}</td>
-            </>
-          ))}
-        </tr>
-      </thead>
-      <tbody />
-    </table>
-  );
-};
+  <table>
+    <thead>
+      <tr>
+        <th>Book Id</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      <tr>
+        {books.map((book) => (
+          <>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.category}</td>
+          </>
+        ))}
+      </tr>
+    </thead>
+    <tbody />
+  </table>
+);
 const mapStateToProps = (state) => ({ books: state.books });
 
 BooksList.propTypes = {
