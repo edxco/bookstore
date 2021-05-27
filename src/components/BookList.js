4b2ff661a8f8340a3ad3d8f72c59014402231867
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-05-27 15:49:12 -0500

    move booklist and bookforms to containers folder

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BooksList = ({ books }) => {
-  console.log(books);
-  return (
+const BooksList = ({ books }) => (
 
-    <table>
+  <table>
 
-      <tr>
-        <th>Book Id</th>
-        <th>Title</th>
-        <th>Category</th>
+    <tr>
+      <th>Book Id</th>
+      <th>Title</th>
+      <th>Category</th>
+    </tr>
+
+    {books.map((book) => (
+
+      <tr key={book.id + 2}>
+        <td key={book.id}>{book.id}</td>
+        <td key={book.title}>{book.title}</td>
+        <td key={book.category}>{book.category}</td>
       </tr>
 
-      {books.map((book) => (
+    ))}
 
-        <tr key={book.id + 2}>
-          {console.log(book.id)}
-          <td key={book.id}>{book.id}</td>
-          <td key={book.title}>{book.title}</td>
-          <td key={book.category}>{book.category}</td>
-        </tr>
-
-      ))}
-
-      <tbody />
-    </table>
-  );
-};
+    <tbody />
+  </table>
+);
 
 BooksList.propTypes = {
   books: PropTypes.objectOf(PropTypes.object).isRequired,
