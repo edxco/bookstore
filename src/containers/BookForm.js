089d9db813a90283ad99de975fa6b6fb9dd2bad4
tavo <gasb_150@hotmail.com>
2021-05-28 16:15:47 -0500

    Create handleChange method

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uniqid from 'uniqid';
 
 const booksCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-const BookForm = () => (
-  <div>
-    <form>
-      <label htmlFor="title">
-        Title
-        <input id="title" name="title" type="text" />
-      </label>
-      <label htmlFor="options">
-        Title
-        <select name="options" id="options">
-          {booksCategories.map((category) => (
-            <option key={category}>
-              {category}
+// store title and book category
+// handle change
+// handle submit
+const BookForm = () => {
+  const [data, setData] = useState({
+    id: uniqid.process(),
+    title: '',
+    category: '',
+  });
+
+  const handleChange = ({ target }) => {
+    setData((oldData) => ({
+      ...oldData, [target.name]: target.value,
+    }));
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">
+          Title
+          <input onChange={handleChange} id="title" name="title" type="text" />
+        </label>
+
+        <label htmlFor="options">
+          Title
+          <select onChange={handleChange} name="category" id="options" value={data.category}>
+            <option value="" disabled>
+              Select Category
             </option>
-          ))}
-        </select>
-      </label>
-      <input type="submit" value="New book" />
-    </form>
-  </div>
-);
+            {booksCategories.map((category) => (
+              <option value={category} key={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </label>
+        <input type="submit" value="New book" />
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
