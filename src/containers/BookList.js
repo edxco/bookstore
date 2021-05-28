b4f57a293bf0ee37f867b7d508ddf4d5ad235ff4
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-05-28 17:03:08 -0500

    DeleteBook action

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,38 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { deleteBook } from '../actions';
 
-const BooksList = ({ books }) => (
+const BooksList = ({ books, deleteBook }) => {
+  const handleRemoveBook = (book) => (
+    deleteBook(book)
+  );
+  return (
 
-  <table>
+    <table>
 
-    <thead>
-      <tr>
-        <th>Book Id</th>
-        <th>Title</th>
-        <th>Category</th>
-      </tr>
-    </thead>
-    <tbody>
-      {books.bookReducer.map((book) => (
-
-        <tr key={book.id + 2}>
-          <td key={book.id}>{book.id}</td>
-          <td key={book.title}>{book.title}</td>
-          <td key={book.category}>{book.category}</td>
+      <thead>
+        <tr>
+          <th>Book Id</th>
+          <th>Title</th>
+          <th>Category</th>
+          <th>Action</th>
         </tr>
+      </thead>
+      <tbody>
+        {books.bookReducer.map((book) => (
 
-      ))}
-    </tbody>
+          <tr key={book.id + 2}>
+            <td key={book.id}>{book.id}</td>
+            <td key={book.title}>{book.title}</td>
+            <td key={book.category}>{book.category}</td>
+            <td>
+              <button type="button" onClick={handleRemoveBook}>Remove</button>
+            </td>
+          </tr>
 
-    <tbody />
-  </table>
-);
+        ))}
+      </tbody>
+
+      <tbody />
+    </table>
+  );
+};
 
 const mapStateToProps = (state) => ({ books: state });
 
 BooksList.propTypes = {
   books: PropTypes.objectOf(PropTypes.array).isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, { deleteBook })(BooksList);
