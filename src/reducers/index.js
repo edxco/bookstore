211b664e402e78c0da843433f030b98d14479f98
tavo <gasb_150@hotmail.com>
2021-05-31 11:12:02 -0500

    Add filter to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import bookReducer from './book';
+import filter from './filter'
 
 const rootReducer = combineReducers({
   bookReducer,
+  filter,
 });
 
 export default rootReducer;
