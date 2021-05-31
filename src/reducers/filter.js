58ac3cd3a0b31bacf38e06acedfc50a3287bc090
tavo <gasb_150@hotmail.com>
2021-05-31 11:25:24 -0500

    Implement filterReducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,11 @@
+const filterReducer = (option = "All", action) => {
+  switch (action.type) {
+    case ('CHANGE_FILTER'):
+      return action.payload;
+    default:
+      return option;
+  }
+
+}
+
+export default filterReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 
 import bookReducer from './book';
-import filter from './filter'
+import filterReducer from './filter'
 
 const rootReducer = combineReducers({
   bookReducer,
-  filter,
+  filterReducer,
 });
 
 export default rootReducer;
